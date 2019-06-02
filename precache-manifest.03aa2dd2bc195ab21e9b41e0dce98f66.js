dd067960e3322064e0edcaca48fb89782daf5f6f
yanghongwei <yanghongwei@boo.chat>
2019-06-02 19:47:22 +0800

    Updates

---
+++ precache-manifest.03aa2dd2bc195ab21e9b41e0dce98f66.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "c8a54cb52b8e21cb40f2",
+    "url": "js/chunk-vendors.f41fe7a1.js"
+  },
+  {
+    "revision": "9b02fde3df02466a494e",
+    "url": "js/app.e03fca0c.js"
+  },
+  {
+    "revision": "94ccf175ec6dfee12c6f38853873e9ee",
+    "url": "index.html"
+  },
+  {
+    "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
+    "url": "img/ionicons.a2c4a261.svg"
+  },
+  {
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "fonts/ionicons.d535a25a.ttf"
+  },
+  {
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "fonts/ionicons.99ac3308.woff"
+  },
+  {
+    "revision": "143146fa24554ae2c5ac0a3982abb952",
+    "url": "fonts/ionicons.143146fa.woff2"
+  },
+  {
+    "revision": "c8a54cb52b8e21cb40f2",
+    "url": "css/chunk-vendors.177f70bf.css"
+  },
+  {
+    "revision": "9b02fde3df02466a494e",
+    "url": "css/app.e5d47b5b.css"
+  }
+];
